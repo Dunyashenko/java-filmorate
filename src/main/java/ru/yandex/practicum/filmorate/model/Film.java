256a12a73f105485685846fa8bf91302839774d9
nikita <nikita.dunyashenko@dif.tech>
2025-09-08 18:32:34 +0300

    checkstyle corrected and added as a plugin to pom

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,11 +2,8 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.time.LocalDate;
 
 @Data
